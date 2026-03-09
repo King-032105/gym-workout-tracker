5dea3d1df52a9c37061e2cab0970c913f7fd2caf
Deliar <deib25@student.bth.se>
2026-03-09 18:14:20 +0100

    Updated trigger to use new pr_log structure

--- gym_tracker_dump.sql
+++ gym_tracker_dump.sql
@@ -104,35 +104,6 @@ LOCK TABLES `workout_exercises` WRITE;
 INSERT INTO `workout_exercises` VALUES (1,1,1,4,8,80.00),(2,1,2,3,10,60.00),(3,1,3,3,12,20.00),(4,2,1,4,6,85.00),(5,2,2,3,8,65.00),(6,2,11,3,12,30.00),(7,3,12,4,8,100.00),(8,3,13,3,10,120.00),(9,3,14,3,12,20.00),(10,4,4,4,5,120.00),(11,4,5,3,8,70.00),(12,4,6,3,10,55.00),(13,5,7,4,8,50.00),(14,5,8,3,15,10.00),(15,5,9,3,12,20.00),(16,6,1,3,10,82.50),(17,6,12,3,10,95.00),(18,6,7,3,10,52.50),(19,7,12,4,8,105.00),(20,7,13,3,10,125.00),(21,8,1,4,8,87.50),(22,8,2,3,10,67.50),(23,8,11,4,10,32.50),(24,9,7,4,8,55.00),(25,9,8,3,15,12.00),(26,10,7,4,8,55.00),(27,10,8,3,15,12.00),(28,11,1,2,3,20.00),(29,11,2,3,4,34.00),(30,12,18,21,4,43.30),(31,13,1,15,3,35.00),(32,15,5,21,2,21.00),(33,16,12,32,2,40.00),(34,17,18,23,3,23.00);
 /*!40000 ALTER TABLE `workout_exercises` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `check_pr` AFTER INSERT ON `workout_exercises` FOR EACH ROW BEGIN
-    DECLARE current_pr DECIMAL(5,2);
-
-    
-    SELECT MAX(weight_kg) INTO current_pr
-    FROM workout_exercises
-    WHERE exercise_id = NEW.exercise_id
-      AND id != NEW.id;
-
-    
-    IF current_pr IS NULL OR NEW.weight_kg > current_pr THEN
-        INSERT INTO pr_log (exercise_id, old_weight, new_weight)
-        VALUES (NEW.exercise_id, current_pr, NEW.weight_kg);
-    END IF;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `workouts`
@@ -168,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-09 17:57:34
+-- Dump completed on 2026-03-09 18:13:56
